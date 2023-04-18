cca7b892f5eee257bb7b6d9796b879d263beb177
Saeid <kp.savadi@gmail.com>
2023-04-18 16:51:22 +0800

    adding bayad center route

--- src/routes/v1/bayadcenter.route.js
+++ src/routes/v1/bayadcenter.route.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const { bayadcenterController } = require('../../controllers');
 const router = express.Router();
-
+const agentLimitation = require('../../middlewares/lb-limitations');
 router
     .route('/billers')
-    .get(bayadcenterController.getBillers);
+    .get(agentLimitation.checkIfUserLimited, bayadcenterController.getBillers);
 
 router
     .route('/billers/:billerId')
-    .get(bayadcenterController.getBiller);
+    .get(bagentLimitation.checkIfUserLimited, ayadcenterController.getBiller);
 
 router
     .route('/billers/:billerId/fees')
-    .get(bayadcenterController.getBillerFees);
+    .get(agentLimitation.checkIfUserLimited, bayadcenterController.getBillerFees);
 
 router
     .route('/billers/:billerId/accounts/:accountId')
-    .get(bayadcenterController.getBillerAccount);
+    .get(agentLimitation.checkIfUserLimited, bayadcenterController.getBillerAccount);
 
 router
     .route('/billers/:billerId/payments')
-    .post(bayadcenterController.createBillerPayment);
+    .post(agentLimitation.checkIfUserLimited, bayadcenterController.createBillerPayment);
 
 router
     .route('/billers/:billerId/payments/:paymentId')
-    .get(bayadcenterController.getBillerPayment);
+    .get(agentLimitation.checkIfUserLimited, bayadcenterController.getBillerPayment);
 
 router
     .route('/wallets')
-    .get(bayadcenterController.getWallets);
+    .get(agentLimitation.checkIfUserLimited, bayadcenterController.getWallets);
 
 module.exports = router;
